b0d22d8715d6c5bcc3376eb9eb69231bb275338b
Anamd-hub <anasmdl120@gmail.com>
2025-04-22 14:47:11 +0300

    Add BlogPost model with title, slug, description, content, img, and published_at

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    content = models.TextField()
+    img = models.URLField()
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
