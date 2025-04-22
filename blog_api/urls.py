2052f10ba0d5f804c38ff446fbcf735054fec8a2
Anamd-hub <anasmdl120@gmail.com>
2025-04-22 15:51:23 +0300

    Integrated with ImageField and add new blog data

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -22,7 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blog/', include('blog.urls')),  # ✅ your blog API lives here
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
